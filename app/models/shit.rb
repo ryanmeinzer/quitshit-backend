39289690000a27bfbeb18e1096f4513b2391c3d9
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-17 15:43:54 -0400

    updated input validations again to only letters and spaces

--- app/models/shit.rb
+++ app/models/shit.rb
@@ -4,5 +4,5 @@ class Shit < ApplicationRecord
     validates :name, presence: true
     validates :name, uniqueness: { case_sensitive: false }
     validates :name, exclusion: { in: %w(fuck shit damn horny bitch cunt ass gay anal balls blowjob clit cock dick dildo fag god homo jizz cum nigger nigga penis vagina pussy queer slut tit twat whore) }
-    # validates :name, format: { with: /\A[a-zA-Z]+\z/ }
+    validates :name, format: { with: /\A[a-z[ ]]+\Z/i }
 end
